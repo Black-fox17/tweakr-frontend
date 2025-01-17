cd7eaeafeb918c3d60097392e05f5fde5e3fc7c1
Shamsudee Zakariyyah <92693681+Cham-X@users.noreply.github.com>
2025-01-17 11:03:16 +0100

    Added: setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {
+  dirname
+} from "path";
+import {
+  fileURLToPath
+} from "url";
+import {
+  FlatCompat
+} from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+  import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
@@ -10,7 +17,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "standard", "plugin:tailwindcss/recommended", "prettier"),
 ];
 
 export default eslintConfig;
