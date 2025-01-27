a704038eb27ac4fdfe55540fa0accd8ab10a5893
Shamsudee Zakariyyah <92693681+Cham-X@users.noreply.github.com>
2025-01-27 06:59:08 +0100

    Added: file validation

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -2,21 +2,67 @@
 
 // import { convertFileToUrl } from '@/lib/utils'
 import Image from 'next/image'
-import React, { useCallback } from 'react'
+import React, { useState, useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { getFileType } from '@/lib/utils'
 
 type FileUploaderProps = {
     files: File[] | undefined,
     onChange: (files: File[]) => void
 }
 
+const allowedExtensions = [
+    "pdf",
+    "doc",
+    "docx",
+    "txt",
+    "xls",
+    "xlsx",
+    "csv",
+    "rtf",
+    "ods",
+    "ppt",
+    "odp",
+    "md",
+    "html",
+    "htm",
+    "epub",
+    "pages",
+    "fig",
+    "psd",
+    "ai",
+    "indd",
+    "xd",
+    "sketch",
+    "afdesign",
+    "afphoto",
+];
+
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
 
 const FileUploader = ({ files, onChange }: FileUploaderProps) => {
+    const [error, setError] = useState<string | null>(null);
+
     const onDrop = useCallback((acceptedFiles: File[]) => {
-        // Do something with the files
-        onChange(acceptedFiles)
-    }, [onChange])
+        const invalidFiles = acceptedFiles.filter((file) => {
+            const { type, extension } = getFileType(file.name);
+            return type === "Undefined" || !allowedExtensions.includes(extension);
+        });
+
+        if (invalidFiles.length > 0) {
+            setError(
+                `Invalid file type. Only the following types are allowed: ${allowedExtensions.join(
+                    ", "
+                )}`
+            );
+            return;
+        }
+
+        setError(null); // Clear any previous errors
+        onChange(acceptedFiles);
+    },
+        [onChange]
+    )
     const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
     return (
@@ -43,10 +89,19 @@ const FileUploader = ({ files, onChange }: FileUploaderProps) => {
                         <p className='text-14-regular'>
                             <span className='text-green-500'>click to upload</span> or drag and drop
                         </p>
-                        <p>PDF, DOCX, ...</p>
+                        <p>Allowed types: PDF, DOCX, TXT, ...</p>
                     </div>
                 </>
             )}
+            <div>
+                {
+                    error && (
+                        <p className="text-red-500 text-sm mt-2">
+                            {error}
+                        </p>
+                    )
+                }
+            </div >
         </div>
     )
 }
