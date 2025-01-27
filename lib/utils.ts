2af968d53b600dbc7da93314e0fcd0759e8a6ed4
Shamsudee Zakariyyah <92693681+Cham-X@users.noreply.github.com>
2025-01-27 01:09:36 +0100

    Removed: validation ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,43 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getFileType = (fileName: string) => {
+  const extension = fileName.split('.').pop()?.toLowerCase();
+
+  if (!extension) return { type: 'other', extension: '' };
+
+  const documentExtensions = [
+    'pdf',
+    'doc',
+    'docx',
+    'txt',
+    'xls',
+    'xlsx',
+    'csv',
+    'rtf',
+    'ods',
+    'ppt',
+    'odp',
+    'md',
+    'html',
+    'htm',
+    'epub',
+    'pages',
+    'fig',
+    'psd',
+    'ai',
+    'indd',
+    'xd',
+    'sketch',
+    'afdesign',
+    'afphoto',
+    'afphoto',
+  ];
+
+
+  if (documentExtensions.includes(extension))
+    return { type: 'document', extension };
+
+  return { type: 'Undefined', extension };
+};
